480abb9d90ca6e271ed81a5a5921204a5081924e
Marina Shapovalova <snyzinka@gmail.com>
2023-03-10 16:52:24 +0300

    Initialcommit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+         int ticketPrice = 15635;
+         int oneBonus = 20;
+
+         int bonus = ticketPrice / oneBonus;
+
+        System.out.println(bonus + " миль(ли) за купленный билет");
+    }
+}
